cf5a9e529eaaee715af4bcc7059244756d235e67
Hans-Christian Otto <c.otto@suora.com>
2021-12-08 12:03:07 +0100

    Cleanup

--- locustfile.py
+++ locustfile.py
@@ -22,11 +22,15 @@ class Purchaser(ShopwareUser):
         auth = Authentication(self.client)
         auth.register()
 
+    # Visit random product listing page
+    # add up to five products to cart
+    # do checkout
     @task
     def order(self):
         url = random.choice(listings)
         productUrls = self.visitProductListingPageAndRetrieveProductUrls(
-            productListingUrl=url)
+            productListingUrl=url
+        )
 
         if len(productUrls) == 0:
             logging.error("No products found on this page")
@@ -46,6 +50,7 @@ class PaginationSurfer(ShopwareUser):
     weight = 30
     wait_time = constant(2)
 
+    # Visit a random product listing page and paginate through 1-3 additional pages
     @task()
     def detail_page(self):
         url = random.choice(listings)
