99a3e24dc600f9aa135d0b8d2fa6a993b85f47f6
Jan Krausenbaum <j.krausenbaum@suora.com>
2022-01-10 14:33:11 +0100

    Add basic setup for report html generation

--- reporting.php
+++ reporting.php
@@ -1,11 +1,24 @@
 <?php
 
 use Symfony\Component\Process\Process;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $htmlFile = __DIR__ . '/report.html';
 $pdfFilename = __DIR__  . '/report.pdf';
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader, [
+    'cache' => __DIR__ . '/cache/twig',
+]);
+
+$data = [];
+
+$reportHtml = $twig->render('report.html.twig', $data);
+
+file_put_contents($htmlFile, $reportHtml);
 
 $process = Process::fromShellCommandline(
     sprintf(
@@ -14,6 +27,7 @@ $process = Process::fromShellCommandline(
         '-B 20mm ' .
         '-L 15mm ' .
         '-R 15mm ' .
+        '--disable-local-file-access --allow ./report/ ' .
         '--encoding "utf8" ' .
         '--minimum-font-size 18 ' .
         '--page-width 23cm ' .
@@ -27,4 +41,4 @@ $process = Process::fromShellCommandline(
 );
 $process->run();
 $exitCode = $process->getExitCode();
-var_dump($exitCode);
+var_dump($process->getErrorOutput());
