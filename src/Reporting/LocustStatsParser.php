89350ef477261b70f7f940e24f1976b0420175de
Benjamin Eberlei <kontakt@beberlei.de>
2022-02-01 21:53:30 +0100

    Refactor LocustStatsParser

--- src/Reporting/LocustStatsParser.php
+++ src/Reporting/LocustStatsParser.php
@@ -55,6 +55,15 @@ class LocustStatsParser
 
         $maxTimestamp = $maxTimestamp->modify('+1 minute');
 
+        return $this->createLocustStats($minTimestamp, $maxTimestamp, $operations);
+    }
+
+    private function createLocustStats(
+        \DateTimeImmutable $minTimestamp,
+        \DateTimeImmutable $maxTimestamp,
+        array $operations
+    ): LocustStats
+    {
         $stats = new LocustStats(
             startDate: $minTimestamp,
             endDate: $maxTimestamp,
@@ -62,7 +71,7 @@ class LocustStatsParser
             pageSummary: [],
         );
 
-        $period = new \DatePeriod($minTimestamp, new \DateInterval('PT1M'), $maxTimestamp);
+        $period = new \DatePeriod($stats->startDate, new \DateInterval('PT1M'), $stats->endDate);
 
         foreach ($operations as $operation => $operationStats) {
             $stats->pageSummary[$operation] = $operationStats['summary'];
@@ -81,72 +90,5 @@ class LocustStatsParser
         }
 
         return $stats;
-    }
-
-    public function parseLocustStats(string $statsFilePath, string $statsHistoryFilePath): LocustStats
-    {
-        $stats = $this->parseStatsHistory($statsHistoryFilePath);
-        $stats->pageSummary = $this->parseStats($statsFilePath);
-        return $stats;
-    }
-
-    private function parseStats(string $statsFilePath)
-    {
-        $handle = fopen($statsFilePath, "r");
-
-        if ($handle === false) {
-            throw new \RuntimeException(sprintf('Could not open stats file at %s', $statsFilePath));
-        }
-
-        // Skip headline
-        $headers = fgetcsv($handle);
-        $stats = [];
-
-        while (($data = fgetcsv($handle)) !== false) {
-            $row = array_combine($headers, $data);
-            $stats[$row['Name']] = $row;
-        }
-
-        return $stats;
-    }
-
-    private function parseStatsHistory(string $statsHistoryFilePath): LocustStats
-    {
-        $stats = [];
-        $handle = fopen($statsHistoryFilePath, "r");
-
-        if ($handle === false) {
-            throw new \RuntimeException(sprintf('Could not open stats file at %s', $statsHistoryFilePath));
-        }
-
-        // Skip headline
-        fgetcsv($handle);
-
-        $minTimestamp = PHP_INT_MAX;
-        $maxTimestamp = 0;
-
-        while (($data = fgetcsv($handle)) !== false) {
-            // The stats CSV sometimes contains null bytes around the timestamp 🤷
-            $timestamp = intval(trim($data[0]));
-            $name = $data[3];
-
-            $minTimestamp = min($minTimestamp, $timestamp);
-            $maxTimestamp = max($maxTimestamp, $timestamp);
-
-            if (!isset($stats[$name])) {
-                $stats[$name] = [];
-            }
-
-            // Use data from 95-percentile
-            $stats[$name][$timestamp] = $data[11];
-        }
-
-        fclose($handle);
-
-        return new LocustStats(
-            pageByTime: $stats,
-            startDate: new \DateTimeImmutable('@' . $minTimestamp, new \DateTimeZone('UTC')),
-            endDate: new \DateTimeImmutable('@' . $maxTimestamp, new \DateTimeZone('UTC')),
-        );
     }
 }
--- tests/Reporting/LocustStatsParserTest.php
+++ tests/Reporting/LocustStatsParserTest.php
@@ -21,12 +21,12 @@ class LocustStatsParserTest extends TestCase
 
         $this->assertCount(2, $stats->pageByTime['overall']);
 
-        $this->assertEquals(883, $stats->pageByTime['overall']['2022-01-28 15:38']->get95PercentileResponseTime());
-        $this->assertEquals(234, $stats->pageByTime['overall']['2022-01-28 15:38']->getMedianResponseTime());
-        $this->assertEquals(409, $stats->pageByTime['overall']['2022-01-28 15:38']->getRequestCount());
+        $this->assertEquals(883, $stats->pageByTime['overall']['2022-01-28 14:38']->get95PercentileResponseTime());
+        $this->assertEquals(234, $stats->pageByTime['overall']['2022-01-28 14:38']->getMedianResponseTime());
+        $this->assertEquals(409, $stats->pageByTime['overall']['2022-01-28 14:38']->getRequestCount());
 
-        $this->assertEquals(835, $stats->pageByTime['overall']['2022-01-28 15:39']->get95PercentileResponseTime());
-        $this->assertEquals(263, $stats->pageByTime['overall']['2022-01-28 15:39']->getMedianResponseTime());
-        $this->assertEquals(60, $stats->pageByTime['overall']['2022-01-28 15:39']->getRequestCount());
+        $this->assertEquals(835, $stats->pageByTime['overall']['2022-01-28 14:39']->get95PercentileResponseTime());
+        $this->assertEquals(263, $stats->pageByTime['overall']['2022-01-28 14:39']->getMedianResponseTime());
+        $this->assertEquals(60, $stats->pageByTime['overall']['2022-01-28 14:39']->getRequestCount());
     }
 }
