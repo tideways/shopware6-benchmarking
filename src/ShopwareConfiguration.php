f63a66914457471037c4e7446fdc4164f3f4ac1c
Benjamin Eberlei <kontakt@beberlei.de>
2022-02-07 10:10:28 +0100

    Remove unused Shopware related info from configs.

--- src/ShopwareConfiguration.php
+++ src/ShopwareConfiguration.php
@@ -8,12 +8,5 @@ class ShopwareConfiguration
         public string $version = '',
         public string $phpVersion = '',
         public string $serverHardware = '',
-        public string $httpCacheLifetime = '',
-        public string $cacheBackend = '',
-        public string $productSearchBackend = '',
-        public string $backgroundQueue = '',
-        public int $storefronts = 1,
-        public int $exports = 0,
-        public array $plugins = [],
     ) {}
 }
